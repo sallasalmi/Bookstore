8e65e1b57b4342910e1dfc13f801275abfec8054
salla salmi <salla.salmi@myy.haaga-helia.fi>
2021-02-10 19:24:17 +0200

    ekat lisäykset

---
+++ src/main/java/hh/swd20/bookstore/domain/Book.java
@@ -0,0 +1,49 @@
+package hh.swd20.bookstore.domain;
+
+
+public class Book {
+	
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private int price;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", Author=" + author + ", Year=" + year + ", Isbn=" + isbn + ", Price=" + price + "]";
+	}
+	
+
+}
